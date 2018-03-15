c2a9bd8d5a95fff75b90c8b1ea459f70df04bca5
yangbinnnn <yangbinnnn@gmail.com>
2018-03-15 13:27:56 +0800

    完善Dockerfile

--- docker/apline/entrypoint.sh
+++ docker/apline/entrypoint.sh
@@ -14,13 +14,15 @@ MYSQL_SCHEMA_DIR=${FALCON_DIR}/schema
 
 echo "start ${FALCON_MOUDLE}" \
 && /usr/bin/redis-server --daemonize yes \
+&& mysql_install_db --user=${MYSQL_USER} --datadir=${MYSQL_DATA_DIR} \
 && /usr/bin/mysqld_safe --user=${MYSQL_USER} --datadir=${MYSQL_DATA_DIR} --nowatch \
+&& sleep 3 \
 && cd ${MYSQL_SCHEMA_DIR} \
-&& mysql -h 127.0.0.1 -u ${MYSQL_USER} < 1_uic-db-schema.sql \
-&& mysql -h 127.0.0.1 -u ${MYSQL_USER} < 2_portal-db-schema.sql \
-&& mysql -h 127.0.0.1 -u ${MYSQL_USER} < 3_dashboard-db-schema.sql \
-&& mysql -h 127.0.0.1 -u ${MYSQL_USER} < 4_graph-db-schema.sql \
-&& mysql -h 127.0.0.1 -u ${MYSQL_USER} < 5_alarms-db-schema.sql
+&& mysql -u ${MYSQL_USER} < 1_uic-db-schema.sql \
+&& mysql -u ${MYSQL_USER} < 2_portal-db-schema.sql \
+&& mysql -u ${MYSQL_USER} < 3_dashboard-db-schema.sql \
+&& mysql -u ${MYSQL_USER} < 4_graph-db-schema.sql \
+&& mysql -u ${MYSQL_USER} < 5_alarms-db-schema.sql \
 && cd ${FALCON_DIR}/dashboard \
 && ./control start \
 && cd ${FALCON_DIR} \
@@ -31,4 +33,4 @@ if [ $errno -ne 0 ] ; then
   exit 1
 fi
 ./open-falcon check
-./open-falcon monitor hbs
+./open-falcon monitor hbs # keep container run
